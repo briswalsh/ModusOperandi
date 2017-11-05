de45b0ec03da500bfa9b6074ec89d28d4b7a9736
kaikue <kvk@pobox.com>
2017-11-05 15:28:14 -0500

    Add voice recognizer script

---
+++ Assets/Scripts/VoiceRecognizer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Speech.Recognition;
+using UnityEngine;
+
+public class VoiceRecognizer : MonoBehaviour {
+
+	// TODO:
+	// enable/disable windows speech recognition through button press
+	// easier way of doing function to process speech
+
+	void Start () {
+		SpeechRecognizer recognizer = new SpeechRecognizer();
+
+		// Create a simple grammar that recognizes "red", "green", or "blue".
+		Choices colors = new Choices();
+		colors.Add(new string[] { "red", "green", "blue" });
+
+		// Create a GrammarBuilder object and append the Choices object.
+		GrammarBuilder gb = new GrammarBuilder();
+		gb.Append(colors);
+
+		Grammar g = new Grammar(gb);
+		//recognizer.LoadGrammar(g);
+		recognizer.LoadGrammar(new DictationGrammar());
+		
+		recognizer.SpeechRecognized +=
+		  new EventHandler<SpeechRecognizedEventArgs>(sre_SpeechRecognized);
+	}
+
+	void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+	{
+		print("Speech recognized: " + e.Result.Text);
+		// process e.Result.Text.ToLower()
+	}
+}
