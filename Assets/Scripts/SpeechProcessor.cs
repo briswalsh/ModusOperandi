631aa3f7b35735e99095a9f09fa8381109c01760
kaikue <kvk@pobox.com>
2017-11-29 19:12:42 -0500

    Clean up speech processor

--- Assets/Scripts/SpeechProcessor.cs
+++ Assets/Scripts/SpeechProcessor.cs
@@ -13,60 +13,55 @@ public class SpeechProcessor : MonoBehaviour {
 	}
 
 	private Dictionary<string, AudioClip> audioDictionary; 
-	private AudioSource audio;
+	private AudioSource audioSrc;
+	Dictionary<string, string> responseMap;
 
-	void Awake(){
+	void Awake()
+	{
 		audioDictionary = new Dictionary<string, AudioClip> ();
-		audio = GetComponent<AudioSource>();
-		audio.enabled = true;
+		audioSrc = GetComponent<AudioSource>();
+		audioSrc.enabled = true;
 
-		AudioClip errorClip = Resources.Load ("didnt_catch_that", typeof(AudioClip)) as AudioClip;
-
+		AudioClip errorClip = Resources.Load("didnt_catch_that", typeof(AudioClip)) as AudioClip;
 		audioDictionary.Add("error", errorClip);
 
-		audioDictionary.Add("who", Resources.Load("alex_russell_info", typeof(AudioClip)) as AudioClip);
-		audioDictionary.Add("weapon", Resources.Load("killed_with_trophy_blunt_force_trauma", typeof(AudioClip)) as AudioClip);
-		audioDictionary.Add("picture", Resources.Load("lawrenceville_community_center_pool_locker_room", typeof(AudioClip)) as AudioClip);
-		audioDictionary.Add("bag",Resources.Load("nothing_in_the_bag", typeof(AudioClip)) as AudioClip);
-		audioDictionary.Add("northriver", Resources.Load("northriver_prep_is_a_high_school", typeof(AudioClip)) as AudioClip);
+		responseMap = new Dictionary<string, string>();
+
+		LoadAudio("who", "alex_russell_info");
+		LoadAudio("victim", "alex_russell_info");
+		LoadAudio("weapon", "killed_with_trophy_blunt_force_trauma");
+		LoadAudio("picture", "lawrenceville_community_center_pool_locker_room");
+		LoadAudio("bag", "nothing_in_the_bag");
+		LoadAudio("northriver", "northriver_prep_is_a_high_school");
+	}
+
+	private void LoadAudio(string word, string clipName)
+	{
+		AudioClip audioClip;
+		if (audioDictionary.ContainsKey(clipName))
+		{
+			audioClip = audioDictionary[clipName];
+		}
+		else
+		{
+			audioClip = Resources.Load(clipName, typeof(AudioClip)) as AudioClip;
+        }
+		audioDictionary.Add(clipName, audioClip);
+		responseMap.Add(word, clipName);
 	}
 
 	public void Process(string text)
 	{
 		Debug.Log("Speech recognized: " + text);
-        /*
-		if (text.ToLower().Contains("ball"))
+		
+		foreach (string word in responseMap.Keys)
 		{
-			Debug.Log("You said ball");
+			if (text.ToLower().Contains(word))
+			{
+				audioSrc.PlayOneShot(audioDictionary[responseMap[word]]);
+				return;
+			}
 		}
-        */
-
-		if(text.ToLower().Contains("who") || text.ToLower().Contains("victim"))
-		{
-			audio.PlayOneShot(audioDictionary["who"]);
-			return;
-		}
-        if (text.ToLower().Contains("weapon"))
-        {
-			audio.PlayOneShot (audioDictionary["weapon"]);
-			return;
-        }
-        if (text.ToLower().Contains("picture"))
-        {
-			audio.PlayOneShot (audioDictionary["picture"]);
-			return;
-        }
-        if (text.ToLower().Contains("bag"))
-		{
-			audio.PlayOneShot (audioDictionary["bag"]);
-			return;
-		}
-        if (text.ToLower().Contains("northriver"))
-        {
-			audio.PlayOneShot(audioDictionary["northriver"]);
-			return;
-        }
-			
-		audio.PlayOneShot (audioDictionary["error"]);
+		audioSrc.PlayOneShot(audioDictionary["error"]);
     }
 }
